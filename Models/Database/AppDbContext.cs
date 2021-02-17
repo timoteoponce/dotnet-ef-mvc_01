75069a2d471e5c60791f6cbf6c29927b266cc881
Timoteo Ponce <timo.slack@gmail.com>
2021-02-17 11:26:25 -0400

    Added automated migrations on startup and added description for self-executable setup

--- Models/Database/AppDbContext.cs
+++ Models/Database/AppDbContext.cs
@@ -7,7 +7,7 @@ namespace web_04_ef_sqlite.Models.Database
 
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
-
+            Database.Migrate();
         }
 
         public DbSet<Student> Students { get; set; }
